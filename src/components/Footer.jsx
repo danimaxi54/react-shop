253f1ba8d2421ce975e5270377bc9149399c4cc1
danimaxi54 <idanil11@mail.ru>
2022-01-09 19:32:43 +0700

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ const Footer = () => {
       <div className='footer-copyright'>
         <div className='container'>
           © {new Date().getFullYear()} Copyright Text
-          <a className='grey-text text-lighten-4 right' href='#!'>
+          <a
+            className='grey-text text-lighten-4 right'
+            href='https://github.com/danimaxi54/react-shop'
+            target='_blank'
+            rel='noreferrer'
+          >
             Repo
           </a>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ const Header = () => {
   return (
     <nav className='green darken-1'>
       <div className='nav-wrapper'>
-        <a className='brand-logo' href='!#'>
+        <a className='brand-logo' href='/'>
           React Shop
         </a>
         <ul className='right hide-on-med-and-down' id='nav-mobile'>
           <li>
-            <a href='!#'>Repo</a>
+            <a
+              href='https://github.com/danimaxi54/react-shop'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Repo
+            </a>
           </li>
         </ul>
       </div>
